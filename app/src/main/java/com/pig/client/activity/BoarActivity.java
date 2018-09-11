4f7e4c5bccb7692cc28e6da09b4cf0fdc3ad487f
AWQi <2282332427@qq.com>
2018-09-11 11:58:18 +0800

    添加xml

--- app/src/main/java/com/pig/client/activity/BoarActivity.java
+++ app/src/main/java/com/pig/client/activity/BoarActivity.java
@@ -34,30 +34,31 @@ public class BoarActivity extends AppCompatActivity {
 
 //                LayoutInflater layoutInflater = LayoutInflater.from(BoarActivity.this); //context
 //                LinearLayout layout = (LinearLayout) layoutInflater.inflate(R.layout.activity_boar_add, null);//自定义的layout
-                final AddBoarLayout layout  = new AddBoarLayout(BoarActivity.this);
-                Dialog dialog = new AlertDialog.Builder(BoarActivity.this)
-                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                //  do  something
-                                layout.getEarlabel();
-                                layout.getPigsty();
-                                layout.getVariety();
-                                layout.getState();
-                                layout.getBirthDay();
-                                layout.getEntergroupday();
 
-                            }
-                        })
-                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                //  do   something
-                            }
-                        })
-                        .setView(layout)
-                        .create();
-                     dialog.show();
+//                final AddBoarLayout layout  = new AddBoarLayout(BoarActivity.this);
+//                Dialog dialog = new AlertDialog.Builder(BoarActivity.this)
+//                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                //  do  something
+//                                layout.getEarlabel();
+//                                layout.getPigsty();
+//                                layout.getVariety();
+//                                layout.getState();
+//                                layout.getBirthDay();
+//                                layout.getEntergroupday();
+//
+//                            }
+//                        })
+//                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                //  do   something
+//                            }
+//                        })
+//                        .setView(layout)
+//                        .create();
+//                     dialog.show();
 //                     dialog.getWindow().setContentView(layout);
 
 
---
+++ app/src/main/java/com/pig/client/util/XMLUtil.java
@@ -0,0 +1,57 @@
+package com.pig.client.util;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class XMLUtil {
+
+    // 解析XML
+ static    public List<String> analysisMapXml(Context context,String fileName) {
+        //获取网络XML数据
+        try {
+            AssetManager assetManager = context.getAssets();
+            InputStream is = assetManager.open(fileName);
+            //解析XMLDOM解析=====================================
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(is);
+            //获取根标签
+            Element element = document.getDocumentElement();
+            Log.i("test", "根标签：" + element.getNodeName());
+            NodeList nodeList = element.getElementsByTagName("info");
+            List<String> infoList = new ArrayList<>();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                //获取单个
+                Element infoElement = (Element) nodeList.item(i);
+                //获取<info>属性id的值
+                int id = Integer.parseInt(infoElement.getAttribute("id"));
+                //获取<info>的值
+                String info = infoElement.getTextContent();
+                infoList.add(info);
+            }
+            return infoList;
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
